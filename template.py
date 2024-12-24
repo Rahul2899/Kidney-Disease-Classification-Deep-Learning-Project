46952ce307474ac2bee1c07592fc057570c3f83f
Rahul2899 <ramrajerahul2@gmail.com>
2024-12-24 16:31:48 +0100

    Folder structure added

---
+++ template.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path
+import logging
+
+# Need to create logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+#writing project name here 
+project_name = 'cnnclassifier'
+
+#List of the files and folder 
+list_of_files = [
+
+    #writing this because i want ci/cd pipeline and writing it inside a folder because without writing inside the folder it will not detect
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath} ")
+
+    else:
+        logging.info(f"{filename} is already exists")
+
+
